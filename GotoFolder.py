f6b4bedd771d926b1377d454cf3d0a009a55427e
zv_odd <zv.odd.101@gmail.com>
2014-08-15 15:55:02 +1000

    Its working... mostly Signed-off-by: zv_odd <zv.odd.101@gmail.com>

--- GotoFolder.py
+++ GotoFolder.py
@@ -3,44 +3,58 @@ import subprocess, os, re
 
 goto_folder_settings = sublime.load_settings("GotoFolder.sublime-settings")
 
-def launch_process(arglist):
-	''' launch detached process '''
-	DETACHED_PROCESS = 0x00000008 #is this windows spesific?
-	print "opening proccess %s" % arglist[0]
-	subprocess.Popen(arglist, close_fds=True, creationflags=DETACHED_PROCESS)
-
-def arg_replacer(arglist):
-	""" 
-	Generate a new arguments list replacing (RegEx)"@\w" with corresponding var.
-	NOTE: Unit tests for prefered implementation are currently failing. 
-	      So you get his instead.
-	"""
-	##TODO proper escape parser
-	escapes = {
-		'd': os.path.dirname(self.view.file_name()),
-		'f': os.path.basename(self.view.file_name()),
-		'@': '@',
-	}
-	newargs = []
-	for arg in arglist:
-		if arg.startswith(r'@'):
-			if len(arg) == 2 and escapes.haskey(arg[1]):
-				escaped = escapes[arg[1]]
-				newargs.append(escaped)
-		else:
-			newargs.append(arg)
-	return newargs
 
 
 class GotoFolderCommand(sublime_plugin.TextCommand):
-	def run(self, edit, mode=None):
+	def run(self, edit, c=None):
+		commandkey = c
 		if self.is_enabled():
-			launchers = goto_folder_settings
-			cmd = arg_replacer(launchers[mode])
-			if launchers.has_key(mode):
-				launch_process(cmd)
+			self.settings = sublime.load_settings("GotoFolder.sublime-settings")
+			commands = self.settings.get("commands")
+			if not commands:
+				print "'GotoFolder.sublime-settings' missing key 'commands', no commands to run."
+				return
+			if commands.has_key(commandkey):
+				cmd = self.arg_replacer(commands[commandkey])
+				self.launch_process(cmd)
 			else:
-				print "Unregonised Mode/Launcher Argument: please set in 'GotoFolder.sublime-settings'"
+				print "Unregonised command: please set in 'GotoFolder.sublime-settings'"
 
 	def is_enabled(self):
 		return self.view.file_name() and len(self.view.file_name()) > 0
+
+	def launch_process(self, arglist):
+		''' launch detached process '''
+		DETACHED_PROCESS = 0x00000008 #is this windows spesific?
+		print "opening proccess %s" % arglist[0]
+		subprocess.Popen(arglist, close_fds=True, creationflags=DETACHED_PROCESS)
+
+	def arg_replacer(self, arglist):
+		""" 
+		Generate a new arguments list replacing (RegEx)"@\w" with corresponding var.
+		NOTE: Unit tests for prefered implementation are currently failing. 
+		      So you get his instead.
+		"""
+		##TODO proper escape parser
+
+		linenumber = 0
+		cursors = self.view.sel()
+		if len(cursors) == 1:
+			point = cursors[0].begin()
+			linenumber = self.view.rowcol(point)
+
+		escapes = {
+			'd': os.path.dirname(self.view.file_name()),
+			'f': os.path.basename(self.view.file_name()),
+			'l': linenumber,
+			'@': '@',
+		}
+		newargs = []
+		for arg in arglist:
+			if arg.startswith(r'@'):
+				if len(arg) == 2 and escapes.has_key(arg[1]):
+					escaped = escapes[arg[1]]
+					newargs.append(escaped)
+			else:
+				newargs.append(arg)
+		return newargs
